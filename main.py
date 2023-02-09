8966b077b3b68fa918896caf87c4b132a3612fb9
unknown <isishskw@gmail.com>
2023-02-09 10:08:51 +0900

    2nd commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 #st
 st.set_page_config(page_title='recommend_series')
-st.markdown('#### レコメンド　アプリ')
+st.markdown('#### レコメンド アプリ')
 
 #データ読み込み
 df_zenkoku = pd.read_pickle('df_zenkoku7879.pickle')
@@ -20,28 +20,65 @@ df_zenkoku2 = df_zenkoku2[(df_zenkoku2['sales'] >= sales_min) & (df_zenkoku2['sa
 #target選定用リスト
 st.write('target得意先の選択')
 df_kita = pd.read_pickle('df_kita7879.pickle')
-target_sales_min = st.number_input('得意先絞込みの為、売上下限を入力', key='target_sales_min', value=2000000)
-df_target = df_kita[df_kita['sales'] >= target_sales_min].index
+cust_text = st.text_input('得意先名の一部を入力', '東京イ')
 
-# selectbox target ***
-target_list = list(df_target)
-target = st.selectbox(
-    'target得意先:',
-    target_list,   
-) 
+target_list = []
+for cust_name in df_kita.index:
+    if cust_text in cust_name:
+        target_list.append(cust_name)
 
-#dfをアイテム列だけに絞る
-df_zenkoku3 = df_zenkoku2.drop(['sales', 'a_price'], axis=1)
+if target_list != '':
+    # selectbox target ***
+    target = st.selectbox(
+        'target得意先:',
+        target_list,   
+    ) 
 
-#相関係数 相関なし0-0.2 やや相関あり0.2-0.4 相関あり 0.4-0.7 強い相関 0.7-
-df_corr = df_zenkoku3.corr()
-st.dataframe(df_corr)
+if target != '':
 
-#targetの売上を整理
-df_zenkoku_temp = df_zenkoku.drop(['sales', 'a_price'], axis=1)
-target_sales = df_zenkoku_temp.loc[target]
-st.write(target_sales)
+    #dfをアイテム列だけに絞る
+    df_zenkoku3 = df_zenkoku2.drop(['sales', 'a_price'], axis=1)
 
-#展示品の選択
+    #相関係数 相関なし0-0.2 やや相関あり0.2-0.4 相関あり 0.4-0.7 強い相関 0.7-
+    df_corr = df_zenkoku3.corr()
+
+    #targetの売上を整理
+    df_zenkoku_temp = df_zenkoku.drop(['sales', 'a_price'], axis=1)
+    df_target_sales = df_zenkoku_temp.loc[target]
+
+    #recomenndリスト作成のための計算
+    #相関係数×シリーズ売上の一覧
+    sim_candidates = pd.Series()
+    for i in range(0, len(df_target_sales.index)):
+        # print('adding sims for ' + target_sales.index[i] + '...')
+        #シリーズ毎に相関表作成
+        sims = df_corr[df_target_sales.index[i]]
+        #相関係数×シリーズ金額
+        sims = sims.map(lambda x: round(x * df_target_sales[i]))
+        sim_candidates = sim_candidates.append(sims)
+
+    #シリーズ毎に合計
+    sim_candidates = sim_candidates.groupby(sim_candidates.index).sum()
+    sim_candidates.sort_values(ascending=False, inplace=True)
+
+    #pointsとsalesをmerge
+    df_simcan = pd.DataFrame(sim_candidates)
+    df_sales = pd.DataFrame(df_target_sales)
+    df_merge = df_simcan.merge(df_sales, left_index=True, right_index=True, how='right')
+    df_merge = df_merge.sort_values(0, ascending=False)
+    df_merge.columns = ['points', 'sales']
+    st.markdown('###### 全シリーズ')
+    st.write(df_merge)
+
+    #展示品の指定
+    tenji_series = st.multiselect(
+        '展示品を選択',
+        df_target_sales.index)
+
+    #展示している商品は削る
+    sim_candidates2 = df_merge.drop(index=tenji_series)
+    st.markdown('###### お薦め展示シリーズ')
+    st.write(sim_candidates2)
+
 
 
